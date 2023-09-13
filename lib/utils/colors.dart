808e74bf619df781361a472abe010dc4fd912321
Seenimohamed <144776468+seenimd@users.noreply.github.com>
2023-09-13 12:54:03 +0530

    second commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+/// Dark Colors
+class DarkColors {
+  static const scaffoldBgColor = Color(0xff042a94);
+  static const sheetBgColor = Color(0xff4d7de8);
+  static const btnBgColor = Color.fromARGB(255, 33, 36, 42);
+  static const leftOperatorColor = Color.fromARGB(255, 218, 190, 46);
+}
+
+/// Light Colors
+class LightColors {
+  static const scaffoldBgColor = Color(0xffFFFFFF);
+  static const sheetBgColor = Color(0xff3c3cf5);
+  static const btnBgColor = Color.fromARGB(255, 255, 255, 255);
+  static const operatorColor = Color(0xffEB6666);
+  static const leftOperatorColor = Color.fromARGB(255, 1, 157, 128);
+}
